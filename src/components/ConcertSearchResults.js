c1d8fa4c59cd89940d3234cc2135fba8a4459462
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-02 22:35:45 -0500

    component to show search results

---
+++ src/components/ConcertSearchResults.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ConcertSearchResults extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+            <>
+                <h1>Search Results</h1>
+            </>
+        );
+    }
+}
+export default ConcertSearchResults;
