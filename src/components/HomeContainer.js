cc4d8af38197852b0cb032ce62fa64d441a47cc9
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-03 00:38:48 -0500

    successful search

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -2,13 +2,15 @@
 import React, { Component } from 'react';
 import SelectList from './SelectList';
 import ConcertSearchResults from './ConcertSearchResults';
+import axios from 'axios'
 
 class HomeContainer extends Component {
-    constructor () {
-        super();
+    constructor (props) {
+        super(props);
         this.state = {
-            searchChoice: 'test'
+            searchResults: null
         };
+        this.makeSearch = this.makeSearch.bind(this)
     }
     makeSearch(e, searchArtist, searchYear) {
         e.preventDefault()
@@ -18,6 +20,22 @@ class HomeContainer extends Component {
            console.log(searchYear)
            let url = 'http://localhost:3000/meta/' + formatArtist + '/' + searchYear
            console.log(url)
+           axios({
+               method: 'GET',
+               url: url,
+               dataType: 'jsonp'
+            
+           }).then((response) => {
+            //    if(response.status === 200) {
+                   this.setState({
+                       searchResults: response.data
+                   }, function() {
+                       console.log(this.state.searchResults)
+                   })
+            //    }
+           }).catch(function(error) {
+               console.log(error)
+           })
        }
     }
     render() {
