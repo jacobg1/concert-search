d58ce793f4b13ed524fddc57312800c4445c9733
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-02 20:33:08 -0500

    add header and footer components

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Helmet } from "react-helmet";
+
+class Header extends Component {
+    render () {
+        return (
+            <>
+              <Helmet>
+                  <title>My Title</title>
+                  <link rel="canonical" href="http://mysite.com/example" />
+              </Helmet>
+
+              <header>
+                  <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      Learn React
+                  </a>
+              </header>
+            </>
+        );
+    }
+}
+
+export default Header;
