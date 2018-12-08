a13ae1877bb236063eb28e9140cc928fb1b66840
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-08 17:28:19 -0500

    passing down props and formatting

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -6,46 +6,55 @@ import axios from 'axios'
 
 class HomeContainer extends Component {
     constructor (props) {
-        super(props);
+        super(props)
         this.state = {
-            searchResults: null
-        };
+            searchResults: null,
+        }
         this.makeSearch = this.makeSearch.bind(this)
     }
     makeSearch(e, searchArtist, searchYear) {
-        e.preventDefault()
+        
+       e.preventDefault()
+       
        if(searchArtist !== '') {
+
            let formatArtist = searchArtist.replace(/ /g, '+')
-           console.log(formatArtist)
-           console.log(searchYear)
+
            let url = 'http://localhost:3000/meta/' + formatArtist + '/' + searchYear
-           console.log(url)
+
            axios({
                method: 'GET',
                url: url,
                dataType: 'jsonp'
-            
            }).then((response) => {
-            //    if(response.status === 200) {
-                   this.setState({
-                       searchResults: response.data
-                   }, function() {
-                       console.log(this.state.searchResults)
-                   })
-            //    }
+
+                this.setState({
+                    searchResults: response.data,
+                })
+        
            }).catch(function(error) {
                console.log(error)
            })
        }
     }
+
     render() {
+
         return (
             <>
                 <h1>{ this.state.searchChoice }</h1>
-                <SelectList
-                  makeSearch={ this.makeSearch }
-                 />
-                <ConcertSearchResults />
+                        
+                        <SelectList
+                            makeSearch={ this.makeSearch }
+                        />
+
+                    { 
+                        this.state && this.state.searchResults && 
+                        
+                            <ConcertSearchResults 
+                                concerts={ this.state.searchResults }
+                            />
+                    }
             </>
         );
     }
