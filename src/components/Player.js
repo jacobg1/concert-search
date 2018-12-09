cadfc16fdbac264429c86523858ba7879afffab6
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-09 15:55:58 -0500

    add player component

---
+++ src/components/Player.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types'
+
+class Player extends Component {
+    render() {
+        return (
+            <>
+                <h1>Player goes here</h1>
+            </>
+        );
+    }
+}
+
+export default Player;
