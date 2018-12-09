f7ba0ba66b902d471a6c7bfabf4539a9878ca0b6
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-09 17:19:29 -0500

    adding track list length

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -8,19 +8,30 @@ class TrackList extends Component {
     // }
 
     componentDidMount () {
-        console.log(this.props.trackList)
+        console.log(this.props.trackList[0])
     }
 
     render() {
+
+        let trackListLength = Object.keys(this.props.trackList).length
+
         const showTrackList = Object.keys(this.props.trackList).map((song, i) => {
             return <div key={i}>
                       <p>
                         <span 
-                            onClick={() => this.props.setSong(this.props.trackList[song].playUrl)}
-                        >play</span>
+                            onClick={() => this.props.setSong( this.props.trackList[song].playUrl, i )}
+                        >play </span>
 
-                          {this.props.trackList[song].title}
+                          {
+                              this.props.trackList[song].title 
+                                ? this.props.trackList[song].title
+                                : this.props.trackList[song].name
+                                ? this.props.trackList[song].name
+                                : ''
+                          }
+                            <span> track { i + 1 } of { trackListLength }</span>
                       </p>
+                      
                    </div>
         })
         return (
