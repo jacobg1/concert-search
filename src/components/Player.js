dcfc20a3cb86596d1f259a3c3cb74c779474647e
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-09 16:17:26 -0500

    passing down play url to play song!

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,30 @@
+/* eslint-disable no-console */
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 class Player extends Component {
+
+    
+    componentDidUpdate () {
+        console.log(this.props.songToPlay)
+
+    }
     render() {
         return (
             <>
-                <h1>Player goes here</h1>
+                <audio 
+                    type='audio/mp3'
+                    controls 
+                    autoPlay 
+                    src={ this.props.songToPlay } 
+                />
             </>
         );
     }
 }
 
 export default Player;
+
+Player.propTypes = {
+    songToPlay: PropTypes.string
+}
