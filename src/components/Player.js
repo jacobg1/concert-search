5d45b2d9749419553c1cfcb94c6389cd020e9acd
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-12 19:10:25 -0500

    refactor

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,13 +5,18 @@ import PropTypes from 'prop-types'
 class Player extends Component {
 
     
-    componentDidUpdate () {
-        // console.log(this.props.songToPlay)
+    // componentDidUpdate(prevProps) {
+    //     // console.log(this.props.songToPlay)
+    //     // if(prevProps.songToPlay !== this.props.songToPlay) {
+    //     //     this.setState({ })
+    //     // }
 
-    }
+    // }
     render() {
         return (
             <>
+                
+                <button onClick={() => this.props.prevSong()}>prev</button>
                 <audio 
                     type='audio/mp3'
                     controls 
@@ -19,6 +24,7 @@ class Player extends Component {
                     src={ this.props.songToPlay } 
                     onEnded={() => this.props.nextSong()}
                 />
+                <button onClick={() => this.props.nextSong()}>next</button>
             </>
         );
     }
@@ -28,5 +34,6 @@ export default Player;
 
 Player.propTypes = {
     songToPlay: PropTypes.string,
-    nextSong: PropTypes.func.isRequired
+    nextSong: PropTypes.func.isRequired,
+    prevSong: PropTypes.func.isRequired
 }
