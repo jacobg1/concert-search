fd69ea76fc4406209aee93fffdee9ef5dc58a4f9
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-12 19:11:41 -0500

    moving logic for playlist to parent component

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,42 +1,91 @@
 /* eslint-disable no-console */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+// import PlayList from './PlayList'
 
 class TrackList extends Component {
-    // constructor(props) {
-    //     super(props)
-    // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            playList: null,
+        }
+    }
 
     componentDidMount () {
         console.log(this.props.trackList[0])
     }
+   
+    // addToPlayList(songIndex) {
+    //     console.log(songIndex)
+    //     let name = this.props.trackList[songIndex].title 
+    //         ? this.props.trackList[songIndex].title 
+    //         : this.props.trackList[songIndex].name 
+    //         ? this.props.trackList[songIndex].name 
+    //         : ''
+
+    //     let newTrack = [{
+    //         name: name,
+    //         songUrl: this.props.trackList[songIndex].playUrl,
+    //         id: songIndex
+
+    //     }]
+    //     let newTrackArray = [...this.state.playList, ...newTrack]
+
+    
+    //     this.setState({
+            
+    //         playList: this.uniq(newTrackArray, 'songUrl')
+    //     }, () => {
+    //         console.log(this.state.playList)
+    //     })
+    //    this.props.setPlayList()
+    // }
+
+    // uniq(a, param) {
+    //     return a.filter(function (item, pos, array) {
+    //         return array.map(function (mapItem) {
+    //             return mapItem[param]
+    //         }).indexOf(item[param]) === pos
+    //     })
+    // }   
 
     render() {
 
-        let trackListLength = Object.keys(this.props.trackList).length
+        let trackListLength = Object.keys(this.props.trackList).length,
+            { checkType } = this.props,
+            { checkSong } = this.props,
+            { trackList } = this.props
 
         const showTrackList = Object.keys(this.props.trackList).map((song, i) => {
             return <div key={i}>
-                      <p>
+                      <p className={( !checkType && i === checkSong ) ? 'active' : '' }>
                         <span 
-                            onClick={() => this.props.setSong( this.props.trackList[song].playUrl, i )}
+                            onClick={() => this.props.setSong( trackList[song].playUrl, i )}
                         >play </span>
 
                           {
-                              this.props.trackList[song].title 
-                                ? this.props.trackList[song].title
-                                : this.props.trackList[song].name
-                                ? this.props.trackList[song].name
+                              trackList[song].title 
+                                ? trackList[song].title
+                                : trackList[song].name
+                                ? trackList[song].name
                                 : ''
                           }
                             <span> track { i + 1 } of { trackListLength }</span>
+                            <span
+                                onClick={() => this.props.setPlayList(i)}
+                            > add to play list</span>
                       </p>
-                      
                    </div>
         })
+        
         return (
             <>
+            <h1>tracklist:</h1>
                 { showTrackList }
+                {/* {
+                    this.state && this.state.playList && 
+                        <PlayList playLisst={ this.state.playList }/>
+                } */}
             </>
         );
     }
@@ -46,5 +95,8 @@ export default TrackList;
 
 TrackList.propTypes = {
     trackList: PropTypes.array.isRequired,
-    setSong: PropTypes.func.isRequired
+    setSong: PropTypes.func.isRequired,
+    setPlayList: PropTypes.func.isRequired,
+    checkSong: PropTypes.number,
+    checkType: PropTypes.bool
 }
