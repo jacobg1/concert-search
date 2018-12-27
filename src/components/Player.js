7bcb17060fc544d129f4ce8bc71b36d795fbdf72
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-27 12:11:17 -0500

    adding some styles to audio tag

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import styles from './styles/Player.module.scss'
 
 class Player extends Component {
 
@@ -8,17 +9,18 @@ class Player extends Component {
         let { playListSongIndex } = this.props,
             { songToPlay } = this.props
         return (
-            <>
+            <div className={ styles.player }>
                 <button onClick={() => this.props.prevSong(playListSongIndex)}>prev</button>
+                <button onClick={() => this.props.nextSong(playListSongIndex)}>next</button>
                 <audio 
                     type='audio/mp3'
                     controls 
+                    controlsList='nodownload'
                     autoPlay 
                     src={ songToPlay } 
                     onEnded={() => this.props.nextSong(playListSongIndex)}
                 />
-                <button onClick={() => this.props.nextSong(playListSongIndex)}>next</button>
-            </>
+            </div>
         );
     }
 }
