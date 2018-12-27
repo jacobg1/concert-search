89ad7d3ea59577d19be44e2903932b6e27b300ff
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-27 17:06:13 -0500

    add music visualization component

---
+++ src/components/Visualizer.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+import React, { Component } from 'react';
+
+class Visualizer extends Component {
+    
+    render() {
+        return (
+            <>
+               <h1>
+                   Visualizer
+               </h1>
+            </>
+        );
+    }
+}
+
+export default Visualizer;
